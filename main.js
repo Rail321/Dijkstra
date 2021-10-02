2ecd7f2df55cdbc5197380efec655e596b6a2e17
Rail Shakurov <shakurov-98@mail.ru>
2021-10-02 19:51:23 +0300

    Fixed links and some changes of js

--- main.js
+++ main.js
@@ -1,90 +1,3 @@
-const graph = [
-  {
-    name: 'A',
-    edges: [
-      {
-        name: 'B',
-        value: 3,
-      },
-      {
-        name: 'C',
-        value: 6,
-      },
-      {
-        name: 'D',
-        value: 1,
-      },
-    ]
-  },
-  {
-    name: 'B',
-    edges: [
-      {
-        name: 'A',
-        value: 3,
-      },
-      {
-        name: 'E',
-        value: 8,
-      },
-    ]
-  },
-  {
-    name: 'C',
-    edges: [
-      {
-        name: 'A',
-        value: 6,
-      },
-      {
-        name: 'D',
-        value: 4,
-      },
-      {
-        name: 'E',
-        value: 4,
-      },
-    ]
-  },
-  {
-    name: 'D',
-    edges: [
-      {
-        name: 'A',
-        value: 1,
-      },
-      {
-        name: 'C',
-        value: 4,
-      },
-      {
-        name: 'E',
-        value: 12,
-      },
-    ]
-  },
-  {
-    name: 'E',
-    edges: [
-      {
-        name: 'B',
-        value: 8,
-      },
-      {
-        name: 'C',
-        value: 4,
-      },
-      {
-        name: 'D',
-        value: 12,
-      },
-    ]
-  },
-]
-
-const start = 'A'
-const end = 'E'
-
 const dijcstra = (graph, start, end) => {
 
   const getNextNode = (graph) => {
@@ -171,6 +84,93 @@ const dijcstra = (graph, start, end) => {
 
   return graph.find(node => node.name === end)
 }
+
+const graph = [
+  {
+    name: 'A',
+    edges: [
+      {
+        name: 'B',
+        value: 3,
+      },
+      {
+        name: 'C',
+        value: 6,
+      },
+      {
+        name: 'D',
+        value: 1,
+      },
+    ]
+  },
+  {
+    name: 'B',
+    edges: [
+      {
+        name: 'A',
+        value: 3,
+      },
+      {
+        name: 'E',
+        value: 8,
+      },
+    ]
+  },
+  {
+    name: 'C',
+    edges: [
+      {
+        name: 'A',
+        value: 6,
+      },
+      {
+        name: 'D',
+        value: 4,
+      },
+      {
+        name: 'E',
+        value: 4,
+      },
+    ]
+  },
+  {
+    name: 'D',
+    edges: [
+      {
+        name: 'A',
+        value: 1,
+      },
+      {
+        name: 'C',
+        value: 4,
+      },
+      {
+        name: 'E',
+        value: 12,
+      },
+    ]
+  },
+  {
+    name: 'E',
+    edges: [
+      {
+        name: 'B',
+        value: 8,
+      },
+      {
+        name: 'C',
+        value: 4,
+      },
+      {
+        name: 'D',
+        value: 12,
+      },
+    ]
+  },
+]
+
+const start = 'A'
+const end = 'E'
 
 const result = dijcstra(graph, start, end)
 
