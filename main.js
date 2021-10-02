4224083e2d856eb69a0f31ba537775edba0ade71
Rail Shakurov <shakurov-98@mail.ru>
2021-10-02 18:55:16 +0300

    Althorytm updated

--- main.js
+++ main.js
@@ -85,60 +85,93 @@ const graph = [
 const start = 'A'
 const end = 'E'
 
-const findMin = nodes => {
-  
-  let min = nodes.find(node => !node.completed)
+const getNextNode = (graph) => {
 
-  if (!min) {
-    return false
-  }
+  let nextNode = graph.find(node => !node.done)
 
-  for (node of nodes) {
+  for (node of graph) {
 
-    if (node.value < min.value && !node.completed) {
-      min = node
+    if (!node.done &&  node.value < nextNode.value) {
+
+      nextNode = node
     }
   }
-    
-  return min
+
+  return nextNode
 }
 
-const getNode = name => {
-  return graph.find(node => node.name === name)
-}
+const getNextEdge = (node) => {
 
-for (node of graph) {
-
-  node.completed = false
-  node.value = node.name === start ? 0 : Infinity
+  let nextEdge = node.edges.find(edge => !edge.done)
 
   for (edge of node.edges) {
-    edge.completed = false
-  }
-}
 
-console.log(graph)
+    if (!edge.done &&  edge.value < nextEdge.value) {
 
-let currentNode = findMin(graph)
-
-while (currentNode) {
-  
-  console.log(currentNode)
-
-  let currentEdge = findMin(currentNode.edges)
-
-  while (currentEdge) {
-
-    getNode(currentEdge.name).value = currentNode.value + currentEdge.value
-    currentEdge.completed = true
-
-    console.log(currentEdge)
-
-    currentEdge = findMin(currentNode.edges)
+      nextEdge = edge
+    }
   }
 
-  currentNode.completed = true
-  currentNode = findMin(graph)
-
-  console.log(graph)
+  return nextEdge
 }
+
+const dijcstra = (graph, start, end) => {
+
+  for (node of graph) {
+
+    node.done = false
+    node.value = node.name === start ? 0 : Infinity
+
+    for (edge of node.edges) {
+
+      edge.done = false
+    }
+  }
+
+  let currentNode
+
+  currentNode = getNextNode(graph)
+
+  while (currentNode) {
+
+    let currentEdge = getNextEdge(currentNode)
+
+    // console.log('currentNode: ', currentNode) // debug
+
+    while (currentEdge) {
+
+      // console.log(' currentEdge: ', currentEdge) // debug
+
+      const targetNode = graph.find(node => node.name === currentEdge.name)
+      const currentValue = currentNode.value + currentEdge.value
+
+      // console.log(' targetNode: ', targetNode) // debug
+      // console.log(' currentValue: ', currentValue) // debug
+
+      if (currentValue < targetNode.value) {
+
+        targetNode.value = currentValue
+
+        // console.log(' targetNode: ', targetNode) // debug
+      }
+
+      // console.log('') // debug
+
+      currentEdge.done = true
+
+      currentEdge = getNextEdge(currentNode)
+    }
+
+    currentNode.done = true
+
+    currentNode = getNextNode(graph)
+  }
+
+  // console.log('graph: ', graph) // debug
+
+  return graph.find(node => node.name === end)
+}
+
+const result = dijcstra(graph, start, end)
+
+console.log(result)
